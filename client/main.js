7a3c8b16a2e87176a9a55de3611567bd7a3d4f76
Molly Rusk <mollyrusk@gmail.com>
2021-12-22 12:31:36 -0700

    add color change to temp page

--- client/main.js
+++ client/main.js
@@ -1,18 +1,18 @@
 
-let tempBtn = document.getElementById('tempBtn')
-tempBtn.addEventListener('click', function(){
-    document.location.href = './temp.html'
-})
+// let tempBtn = document.getElementById('tempBtn')
+// tempBtn.addEventListener('click', function(){
+//     document.location.href = './temp.html'
+// })
 
-let lockBtn = document.getElementById('lockBtn')
-lockBtn.addEventListener('click', function(){
-    document.location.href = './lock.html'
-})
+// let lockBtn = document.getElementById('lockBtn')
+// lockBtn.addEventListener('click', function(){
+//     document.location.href = './lock.html'
+// })
 
-let lightBtn = document.getElementById('lightBtn')
-lightBtn.addEventListener('click', function(){
-    document.location.href = './lights.html'
-})
+// let lightBtn = document.getElementById('lightBtn')
+// lightBtn.addEventListener('click', function(){
+//     document.location.href = './lights.html'
+// })
 
 let tempSubmitForm = document.getElementById('tempForm')
 let tempInput = document.getElementById('tempInput')
@@ -20,5 +20,14 @@ let tempHeader = document.getElementById('tempHeader')
 
     tempSubmitForm.addEventListener('submit', function(event) {
         event.preventDefault()
-        tempHeader.innerText = 'The current temperature is ' + tempInput.value
+        tempHeader.innerText = 'The current temperature is ' + tempInput.value + ' °F'
+        
+        if(tempInput.value >= 75){
+           document.getElementById('tempBody').style.background = '#f58235'
+        }
+        else {
+            document.getElementById('tempBody').style.background = '#93dcf7'
+        }
     })
+
+    
