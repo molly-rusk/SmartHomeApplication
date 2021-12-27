976058ec22cffd8b50c45a7b594be6c5439b47ce
Molly Rusk <mollyrusk@gmail.com>
2021-12-27 13:09:42 -0700

    made changes to lock page

--- server/index.js
+++ server/index.js
@@ -6,6 +6,25 @@ app.use(cors())
 app.use(express.json())
 
 
+app.get("/api/locked", (req,res) => {
+    const locked = ["All doors have been locked",
+                    "Doors locked successfully",
+                    ]
 
+    let randomIndex = Math.floor(Math.random() * locked.length)
+    let randomLocked = locked[randomIndex]
+    res.status(200).send(randomLocked)
+})
+
+app.get("/api/unlocked", (req,res) => {
+    const unlocked = ["All doors have been unlocked sucessfully",
+                      "Doors unlocked sucessfully"
+                    ]
+    
+    let randomIndex = Math.floor(Math.random() * unlocked.length)
+    let randomUnlocked = unlocked[randomIndex]
+
+    res.status(200).send(randomUnlocked)
+})
 
 app.listen(3030, () => console.log('Listening on port 3030!'))
